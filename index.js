fbd89654e60d7cd18ed79c1e05e1554096cf7d10
Iva <iva_dkostadinova@abv.bg>
2024-04-08 14:02:49 +0300

    create folder public and add resourses

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express= require('express')
 const app=express();
 
-app.get('/',(req, res)=>{
-res.send('Hello World')
-});
+const routes=require('./routes')
 
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}))
+
+
+app.use(routes)
 
 app.listen(5000, ()=>console.log(`App is listening on http://localhost:5000`))

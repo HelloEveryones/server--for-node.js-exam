38200589352faae8cced20e250d09b3faf1d5840
Iva <iva_dkostadinova@abv.bg>
2024-04-08 14:53:58 +0300

    install and set up mongoose

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express= require('express')
 const app=express();
+const mongoose= require('mongoose');
 
 const routes=require('./routes');
 const handlebars=require('express-handlebars');
@@ -12,6 +13,19 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs')
 
 
-app.use(routes)
+app.use(routes);
+
+mongoose.connect('mongodb://127.0.0.1/course-book');
+mongoose.connection.on('connected', ()=>{
+    console.log('DB is connected');
+})
+
+mongoose.connection.on('Error', (err)=>{
+    console.log(err);
+});
+
+mongoose.connection.on('disconnected', ()=>{
+    console.log('DB is disconnected');
+})
 
 app.listen(5000, ()=>console.log(`App is listening on http://localhost:5000`))

3bf77d4902aa7891c006af17a1a2736d1ae3f573
Iva <iva_dkostadinova@abv.bg>
2024-04-09 11:20:24 +0300

    Login

--- models/User.js
+++ models/User.js
@@ -1,21 +1,23 @@
-const mongoose= require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const userSchema= new mongoose.Schema({
-
-    username:{
-       type: String,
-       required: true,
-    },
-    email:{
-        type: String,
-        required:true,
-    },
-    password: {
-        type: String,
-        required: true,
-    }
-
-})
-
-const User= mongoose.model('User', userSchema);
-module.exports=User;
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    unique:true
+  },
+});
+userSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 12);
+});
+const User = mongoose.model("User", userSchema);
+module.exports = User;
---
+++ services/authService.js
@@ -0,0 +1,46 @@
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+const jwt=require('../lib/jsonwebtoken');
+const SECRET='79504kfngkdgfjssctfr5667v'
+
+
+exports.register = async (userData) => {
+  if (userData.password !== userData.rePassword) {
+    throw new Error(`Password mismatch`);
+  }
+  const user = await User.findOne({ email: userData.email });
+  if (user) { 
+    throw new Error(`User already exist!`);
+  }
+  const createdUser=await User.create(userData);
+
+  const token=await generateToken(createdUser);
+  return token;
+};
+exports.login = async ({ email, password }) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error(`Email or password is invalid`);
+  }
+  const isValid = await bcrypt.compare(password, user.password);
+
+  if (!isValid) {
+    throw new Error(`Email or password is invalid`);
+  }
+
+  const token=await generateToken(user);
+  return token;
+};
+
+function generateToken(user){
+  const payload={
+    _id:user._id,
+    user:user.username,
+    email:user.email,
+  }
+
+  return jwt.sign(payload, SECRET,{expiresIn:"2h"});
+
+  
+}
